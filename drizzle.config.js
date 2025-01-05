dbbfc1e57556effded2470472b9c2457a4e883de
Utkarsh--eng <utkarshkryadav93@gmail.com>
2025-01-06 01:31:53 +0530

    first commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://fintrack_owner:0MTXrh8ULCzK@ep-autumn-fog-a5mgz5vg.us-east-2.aws.neon.tech/fintrack?sslmode=require',
+      url: 'postgresql://fintrack_owner:0MTXrh8ULCzK@ep-autumn-fog-a5mgz5vg.us-east-2.aws.neon.tech/budget?sslmode=require',
     }
   };
   
